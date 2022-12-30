3779154aa73c4e4587df03e79ec908238c1f2ddb
Lucas Martins <lucas2zonta@hotmail.com>
2022-12-30 11:04:46 -0300

    Atualizando a ação de click para adicionar ao carrinho (array)

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const query = (el)=> document.querySelector(el);
 const queryA = (el)=> document.querySelectorAll(el);
 
+
+let modalQt = 1; //variavel para a qtde INICIAL de pizzas
+let cart = []; //array do carrinho de compras
+let modalKey = 0;
+
 //listar todas as pizzas
 pizzaJson.map((item, index) => {
     //gerando uma copia (clone) da div pizza-item
@@ -16,15 +21,29 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     
-    //trazer as informações da pizza que está sendo clicado
+    //trazer as informações da pizza que está sendo clicado (QUANDO VOCE CLICAR NO + PARA ADD UMA PIZZA E ABRIR O MODAL)
     pizzaItem.querySelector('.pizza-item a').addEventListener('click',(e)=>{
         e.preventDefault(); //desativar as ações default do a
-
-        let key = e.target.closest('.pizza-item').getAttribute('data-key');
         
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+        modalKey = key;
+
         query('.pizzaBig img').src = pizzaJson[key].img;
         query('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         query('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        query('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2).replace('.',',') }`;
+        //remover o selected caso ele esteja selecionado em um tamanho diferente do GRANDE
+        query('.pizzaInfo--size.selected').classList.remove('selected');
+
+        //um foreach com o selectorAll para percorrer os 3 diferentes tamanhos de pizzas
+        queryA('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex === 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex] ;
+        });
+        query('.pizzaInfo--qt').innerHTML = modalQt;
 
         query('.pizzaWindowArea').style.opacity = 0;
         query('.pizzaWindowArea').style.display = 'flex';
@@ -37,5 +56,60 @@ pizzaJson.map((item, index) => {
     //exibir as informações
     query('.pizza-area').append( pizzaItem );
 });
+
+//Eventos do MODAL
+function closeModal() {
+    query('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=> {
+        //para fazer o efeito de fechar o modal
+        query('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
+//quando ele clicar em fechar ou voltar
+queryA('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+
+//quando ele clicar em menos (-) na quantidade de pizzas
+query('.pizzaInfo--qtmenos').addEventListener('click',()=>{
+    if(modalQt > 1) {
+        modalQt--;
+        query('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+
+//quando ele clicar em mais (+) na quantidade de pizzas
+query('.pizzaInfo--qtmais').addEventListener('click',()=>{
+    modalQt++;
+    query('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+//um foreach com o selectorAll para percorrer os 3 diferentes tamanhos de pizzas para adc a class no tamanho clicado
+queryA('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click',(e)=>{
+        query('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
+
+//ação de adicionar a pizza ao clicar em "Adicionar ao Carrinho"
+query('.pizzaInfo--addButton').addEventListener('click', ()=> {
+    //reunir as informações de: qual a pizza, o tamanho selecionado e a quantidade de pizzas
+
+    let size = parseInt(query('.pizzaInfo--size.selected').getAttribute('data-key'));
+    //vai adc um OBJETO no array com o push
+    cart.push({
+        id: pizzaJson[modalKey].id,
+        size: size,
+        qt: modalQt
+    })
+
+    console.log(cart);
+    
+    closeModal();
+
+});
+
 
 
